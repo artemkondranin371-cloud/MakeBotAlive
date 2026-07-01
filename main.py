c1ddfffe8d226dd181405e3b6065cc7e53f334d8
artemkondranin371-cloud <artemkondranin371@gmail.com>
2026-07-01 14:49:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+print("Бот запускается...")
+import telebot
+from flask import Flask
+from threading import Thread
+import time
 
+TOKEN = "8862678941:AAHUGSVtp2qbCT3DSCMBqpvMsjb-JtX1EQY"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я на Render!")
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Бот жив!"
+
+def keep_alive():
+    Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
+
+if __name__ == "__main__":
+    keep_alive()
+    print("Бот запущен, жду сообщения...")
+    bot.polling()
